c585f7a9fca0d4b5be2469c2c3f492173f1b4be4
Rodrigo Coelho <rodrigo@kria.com.br>
2020-12-28 03:21:54 -0300

    Primeiro commit.

---
+++ Sofisa.Desafio.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sofisa.Desafio.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
